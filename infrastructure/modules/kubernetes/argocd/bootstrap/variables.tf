8243dc63d442c41fd2441e703474233778ee0235
Arnaud Maillet <62292183+arnaudmaillet@users.noreply.github.com>
2026-03-22 01:19:14 +0100

    Feat/profile/stress test final (#307)
    * feat(infra): fix profile apps status
    
    * feat(infra): fix profile apps status
    
    * feat(infra): fix profile apps status
    
    ---------
    
    Co-authored-by: arnaudmaillet <arnaud.maillet.pro@gmail.com>

--- infrastructure/modules/kubernetes/argocd/bootstrap/variables.tf
+++ infrastructure/modules/kubernetes/argocd/bootstrap/variables.tf
@@ -1,16 +1,17 @@
 # infrastructure/modules/kubernetes/argocd/bootstrap/variables.tf
 
-variable "repository_url"   { type = string }
-variable "target_revision"  { type = string }
-variable "cluster_name"     { type = string }
+variable "region" { type = string }
+variable "env" { type = string }
+variable "repository_url" { type = string }
+variable "target_revision" { type = string }
+variable "vpc_id" { type = string }
+variable "cluster_name" { type = string }
+variable "cluster_endpoint" { type = string }
 variable "ssl_certificate_arn" { type = string }
 variable "addons_iam_roles" { type = map(string) }
-variable "cluster_endpoint"       { type = string }
-variable "env"                     { type = string }
-variable "vpc_id"                 { type = string }
 
-variable "server_dependency" { 
-  type    = any 
-  default = null
+variable "server_dependency" {
+  type        = any
+  default     = null
   description = "Permet de forcer l'attente de l'installation du serveur ArgoCD"
 }
--- infrastructure/modules/kubernetes/argocd/variables.tf
+++ infrastructure/modules/kubernetes/argocd/variables.tf
@@ -1,15 +1,18 @@
 # infrastructure/modules/kubernetes/argocd/variables.tf
 
-variable "cluster_name"           { type = string }
-variable "cluster_endpoint"       { type = string }
-variable "cluster_ca_certificate" { type = string }
-variable "argocd_version"         { type = string }
+variable "argocd_version" { type = string }
 
-variable "repository_url"         { type = string }
-variable "target_revision"        { type = string }
-variable "env"                     { type = string }
-variable "vpc_id"                 { type = string }
-variable "addons_iam_roles"        { type = map(string) }
-variable "ssl_certificate_arn"     { type = string }
-variable "addons"                  { type = any }
-variable "tags"                    { type = map(string) }
+variable "env" { type = string }
+variable "region" { type = string }
+variable "repository_url" { type = string }
+variable "target_revision" { type = string }
+
+variable "cluster_name" { type = string }
+variable "cluster_endpoint" { type = string }
+variable "cluster_ca_certificate" { type = string }
+
+variable "vpc_id" { type = string }
+variable "addons_iam_roles" { type = map(string) }
+variable "ssl_certificate_arn" { type = string }
+variable "addons" { type = any }
+variable "tags" { type = map(string) }
